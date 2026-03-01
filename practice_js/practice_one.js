43539e2e7f2846c00198b2fe530ad67a3c95792e
Andrey <sevostyanov1999@gmail.com>
2026-03-01 18:37:51 +0300

    объекты

--- practice_js/practice_one.js
+++ practice_js/practice_one.js
@@ -21,17 +21,17 @@
 
 // задание
 // (1)
-let user = {};
+// let user = {};
 
-user.name = "John";
+// user.name = "John";
 
-user.surname = "Smith";
+// user.surname = "Smith";
 
-user.name = "Pete";
+// user.name = "Pete";
 
 // console.log(user);
 
-delete user.name;
+// delete user.name;
 
 // (2)
 let schedule = {};
@@ -107,3 +107,102 @@ let infoAdmin = {
 Object.assign(infoAdmin, infoUsers);
 
 console.log(infoAdmin);
+
+// Методы объекта
+
+let user = {
+    name: 'Jonh',
+    age: 32,
+    sayHi() {
+        console.log(this.name);
+    }
+};
+
+// user.sayHi = function() {
+//     console.log('Привет');
+// }
+
+
+user.sayHi();
+
+// Задание
+// (1)
+let calculator = {
+    numOne: 0,
+    numTwo: 0,
+
+    sum() {
+        return this.numOne + this.numTwo;
+    },
+
+    mul() {
+        return this.numOne * this.numTwo;
+    }
+};
+
+calculator.numOne = 10;
+calculator.numTwo = 20;
+
+console.log(calculator.sum());
+console.log(calculator.mul());
+
+// (2)
+
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep: function() { // показывает текущую ступеньку
+      console.log( this.step );
+      return this;
+    }
+};
+
+ladder.up().up().up().up().down().showStep();
+
+// конструктор, оператор "new"
+
+function User(name) {
+    this.name = name;
+    this.isAdmin = false;
+}
+
+let newUser = new User("Andrey");
+
+console.log(newUser.name);
+
+// Задание
+// (1)
+
+function Calculator() {
+    this.readOne = 0;
+    this.readTwo = 0;
+    this.sum = function() {
+        return this.readOne + this.readTwo;
+    }
+    this.mul = function() {
+        return this.readOne * this.readTwo;
+    }
+}
+
+let newCalculator = new Calculator();
+
+newCalculator.readOne = 100;
+newCalculator.readTwo = 20;
+console.log(newCalculator.sum());
+console.log(newCalculator.mul());
+
+// опциональная цепочка
+
+let newSecondUser = {
+    address: 'Lenina st',
+    id: Symbol("id")
+};
+
+console.log(newSecondUser.address?.street?.home);
